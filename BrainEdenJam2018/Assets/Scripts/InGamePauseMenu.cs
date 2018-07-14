128b0436e371159c79ee500cc379d6e1ad919fc5
filippol <pippo.libardi@gmail.com>
2018-07-14 17:08:58 +0100

    Merged fixed pause menu

--- BrainEdenJam2018/Assets/Scripts/InGamePauseMenu.cs
+++ BrainEdenJam2018/Assets/Scripts/InGamePauseMenu.cs
@@ -1,43 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class InGamePauseMenu : MonoBehaviour {
+public class InGamePauseMenu : MonoBehaviour
+{
 
-	void Start () {
+    public GameObject menu;
 
-		GetComponent<Canvas> ().enabled = false;
-	}
+    void Start()
+    {
 
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.P)) {
-			Time.timeScale = 0;
-			GetComponent<Canvas> ().enabled = !GetComponent<Canvas> ().enabled;
-		}
+        menu.SetActive(false);
+    }
 
-		if (!GetComponent<Canvas> ().enabled) {
-			GameObject.FindGameObjectWithTag ("Player").GetComponent<SmoothMouseLook> ().enabled = true;
-			Cursor.lockState = CursorLockMode.Locked;
-			Time.timeScale = 1;
-		} else {
-			Cursor.lockState = CursorLockMode.None;
-			GameObject.FindGameObjectWithTag ("Player").GetComponent<SmoothMouseLook> ().enabled = false;
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (menu.activeSelf)
+            {
+                menu.SetActive(false);
+                GameObject.FindGameObjectWithTag("Player").GetComponent<SmoothMouseLook>().enabled = true;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                menu.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<SmoothMouseLook>().enabled = false;
+            }
+        }
+    }
 
-	public void BackToPlay(){
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<SmoothMouseLook> ().enabled = true;
-		Cursor.lockState = CursorLockMode.Locked;
-		Time.timeScale = 1;	
-	}
+    public void BackToPlay()
+    {
+        menu.SetActive(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<SmoothMouseLook>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-	public void GoToMainMenu(){
-		Time.timeScale = 1;	
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
-	}
-		
-	public void QuitGame(){
-		Application.Quit ();
-	}
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
