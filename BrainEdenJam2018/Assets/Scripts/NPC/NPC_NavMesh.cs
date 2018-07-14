d55697102b49b6ba6c55372647ea5f6d14473a0b
filippol <pippo.libardi@gmail.com>
2018-07-14 23:59:09 +0100

    Added new textures and level design

---
+++ BrainEdenJam2018/Assets/Scripts/NPC/NPC_NavMesh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_NavMesh : MonoBehaviour {
+
+    public Transform m_target;
+
+    NavMeshAgent m_agent;
+
+
+	// Use this for initialization
+	void Start () {
+        m_agent = this.GetComponent<NavMeshAgent>();
+        if (!m_agent)
+        {
+            Debug.Log("No Aagent Found");
+            return;
+        }
+
+	}
+
+    private void SetDirection()
+    {
+        if (m_target)
+        {
+            Vector3 t_destination;
+
+            t_destination = m_target.transform.position;
+
+            m_agent.SetDestination(t_destination);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (m_agent)
+        {
+            SetDirection();
+        }
+    }
+}
