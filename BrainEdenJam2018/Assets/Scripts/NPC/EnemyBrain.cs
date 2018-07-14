272530e5d348fce7f54cbafcf8d049e150211af5
filippol <pippo.libardi@gmail.com>
2018-07-14 10:13:24 +0100

    Added NPC movements: Wander (to be improved), avoid, seek and flock

--- BrainEdenJam2018/Assets/Scripts/NPC/EnemyBrain.cs
+++ BrainEdenJam2018/Assets/Scripts/NPC/EnemyBrain.cs
@@ -1,102 +1,204 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
 
-public class EnemyBrain : MonoBehaviour
-{
+public class EnemyBrain : MonoBehaviour {
 
     /// <summary>
     /// Variables to hold states of FSM, used to swap a Coroutine (state) with another.
     /// </summary>
-	IEnumerator _currState;
-    IEnumerator _nextState;
+	IEnumerator m_currState;
+	IEnumerator m_nextState;
 
-    public List<Vector3> m_steerings = new List<Vector3>();
+    /// <summary>
+    /// A List of transform of all the closest enemies
+    /// </summary>
+	[SerializeField] private List <Transform> m_neighbors = new List <Transform>();
 
-    void Start()
-    {
+    /// <summary>
+    /// The transform of the Leader to be followed when flocking
+    /// </summary>
+	private Transform m_leader;
+
+    /// <summary>
+    /// Maximum number of a single flock
+    /// </summary>
+	private int m_max_flock = 15;
+
+
+
+	public List <Transform> Neighbors{		
+		get { return m_neighbors; }
+	}
+
+	public Transform Leader{		
+		get { return m_leader; }
+	}
+
+	void Start () {
         ///First coroutine will always be the move state
-		_currState = Moving();
-        StartCoroutine(StateMachine());
-    }
+		m_currState = Moving ();	
+		StartCoroutine(StateMachine()); 
+	}
 
     /// <summary>
     /// Moving state will have Avoid Input and Wander Input
     /// </summary>
-	IEnumerator Moving()
-    {
-        Debug.Log("There");
+	IEnumerator Moving(){
+		GetComponent<MovementManager> ().ClearSteerings ();
+		GetComponent<MovementManager> ().AddSteering(GetComponent<AvoidInput>());
+		GetComponent<MovementManager> ().AddSteering(GetComponent<WanderInput>());
+        gameObject.GetComponentInChildren<Renderer>().material.color = Color.green;
 
-        yield return null;
-    }
+        while (m_nextState == null) {
+			if (Input.GetKeyDown(KeyCode.Tab)) {
+				m_nextState = Alert ();
+			}
+            if (m_neighbors.Count > 0)
+            {
+                m_nextState = Flock();
+            }
+            yield return null;
+		}
+	}
 
     /// <summary>
-    /// Flock state will have avoid input, follow to follow the leader and separation to keep distance from other members of the flock
+    /// Alert State will be entered when a bullet has been shot nearby
     /// </summary>
-	IEnumerator Flock()
-    {
-        yield return null;
-    }
+	IEnumerator Alert(){
+		GetComponent<MovementManager> ().ClearSteerings ();
+		GetComponent<MovementManager> ().AddSteering(GetComponent<AvoidInput>());
+		GetComponent<MovementManager> ().AddSteering(GetComponent<EvadeInput>());
+		while (m_nextState == null) {
+           
+
+            yield return null;
+		}
+	}
+
+    /// <summary>
+    /// Flock state will have avoid input, follow to follow the Leader and separation to keep distance from other members of the flock
+    /// </summary>
+	IEnumerator Flock(){
+		GetComponent<MovementManager> ().ClearSteerings ();
+		GetComponent<MovementManager> ().AddSteering(GetComponent<AvoidInput>());
+		GetComponent<MovementManager> ().AddSteering(GetComponent<FollowInput>());
+		GetComponent<MovementManager> ().AddSteering(GetComponent<SeparationInput>());
+		while (m_nextState == null) {
+            Debug.Log("FLOCKING");
+			if (m_neighbors.Count > m_max_flock || m_leader == null) {
+				m_leader = null;
+				m_neighbors.Clear();
+				m_nextState = Idling (); //change state
+			}
+			
+			yield return null;
+		}
+
+	}
 
     /// <summary>
     /// Attack will only be entered from leaders and will seek at full speed the player
-    /// </summary>
-	IEnumerator Attack()
-    {
-        yield return null;
-    }
+    /// </summary>      
+	IEnumerator Attack(){
+
+		GetComponent<MovementManager> ().ClearSteerings ();
+		GetComponent<MovementManager> ().AddSteering(GetComponent<AvoidInput>());
+		GetComponent<MovementManager> ().AddSteering(GetComponent<SeekInput>());
+		gameObject.GetComponentInChildren<Renderer> ().material.color = Color.red;
+
+		while (m_nextState == null) {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                m_nextState = Moving();
+            }
+
+            yield return null;
+		}
+
+	}
 
     /// <summary>
-    /// Idling is actually just a standby for 1.5 to 3 seconds, then the agent will go back to moving
+    /// Idling is actually just a standby for 1.5 seconds, then the agent will go back to moving
     /// </summary>
-	IEnumerator Idling()
-    {
-        while (_nextState == null)
-        {
+	IEnumerator Idling(){
+		GetComponent<MovementManager> ().ClearSteerings ();
+		while (m_nextState == null) {
 
-            yield return new WaitForSeconds(Random.Range(1.5f, 3));
-            _nextState = Moving();
-            yield return null;
-        }
-    }
+			yield return new WaitForSeconds (1.5f);
+			m_nextState = Moving ();
+			yield return null;
+		}
+	}
 
     /// <summary>
     /// The actual StateMachine will handle the state swap and loop
     /// </summary>
-	IEnumerator StateMachine()
-    {
-        while (_currState != null)
-        {
-            yield return StartCoroutine(_currState);
-            _currState = _nextState;
-            _nextState = null;
-        }
-    }
+	IEnumerator StateMachine(){
+		while (m_currState != null) {
+			yield return StartCoroutine (m_currState);
+			m_currState = m_nextState;
+			m_nextState = null;
+		}
+	}
 
     /// <summary>
     /// All the collision is checked in here and states are swapped accordingly
     /// </summary>
-	private void OnTriggerEnter(Collider coll)
-    {
-        
-    }
+	private void OnTriggerEnter(Collider coll){
+		if (coll.tag != null) {
+            if (!gameObject.CompareTag("Leader"))
+            {
+                if (coll.CompareTag("Enemy"))
+                {
+                    m_neighbors.Add(coll.transform);
+                }
+                else if (coll.CompareTag("Leader"))
+                {
+                    m_leader = coll.transform;
+                }
+            }
+            else
+            {
+
+            }
+		}
+	}
 
     /// <summary>
-    /// If an enemy is near the agent but is not flocking, then dont add it to the neighbors count
+    /// If an enemy is near the agent but is not flocking, then dont add it to the Neighbors count
     /// Otherwise do add it.
     /// </summary>
-	private void OnTriggerStay(Collider coll)
-    {
+	private void OnTriggerStay(Collider coll){
 
+        //if (gameObject.CompareTag("Enemy"))
+        //{
+        //    if (coll.CompareTag("Enemy"))
+        //    {
+        //        if (coll.GetComponent<EnemyBrain>().Leader == null)
+        //        {
+        //            if (m_neighbors.Contains(coll.transform))
+        //            {
+        //                m_neighbors.Remove(coll.transform);
+        //            }
+        //        }
+        //    }
+        //}
     }
 
     /// <summary>
-    /// If the leader has left, top flocking and start Moving around
+    /// If the Leader has left, top flocking and start Moving around
     /// </summary>
 	private void OnTriggerExit(Collider coll)
     {
-        
+        if (coll.tag != null)
+        {
+            if (m_neighbors.Contains(coll.transform))
+            {
+                m_neighbors.Remove(coll.transform);
+            }
+        }
     }
-
 }
